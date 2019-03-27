0c40db4a593d64af34e797e6f6a1dac46483bc18
luoyu <luoyu_bie@sina.com>
2019-03-27 17:36:36 +0800

    #12#

--- Fluent_python/test.py
+++ Fluent_python/test.py
@@ -11,4 +11,21 @@ class Vector:
     def __repr__(self):
         return 'Vector(%r,%r)'%(self.x,self.y)
 
+    def __abs__(self):
+        return hypot(self.x,self.y)
 
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x,y)
+
+    def __mul__(self,scalar):
+        return Vector(self.x * scalar , self.y * scalar)
+
+
+
+
+# rimvalue.cn 
