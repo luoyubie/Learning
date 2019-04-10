8832873f14bbe04ad935541ef232f025008ae419
luoyu <luoyu_bie@sina.com>
2019-04-10 14:22:44 +0800

    #1#

--- Fluent_python/Note.py
+++ Fluent_python/Note.py
@@ -3,5 +3,18 @@
 # @Time: 2019年04月10日
 
 
+"""
+# 斐波拉契数列
+def fib(max):
+    n,a,b = 0,0,1
+    while n<max:
+    # 生成器
+        yield b
+        a,b = b,a+b
+        n += 1
+    return 'done'
 
+for i in fib(10):
+    print(i)
+"""
 
--- Fluent_python/test.py
+++ Fluent_python/test.py
@@ -2,14 +2,5 @@
 # @Author: Bie
 # @Time: 2019年03月12日
 
-def fib(max):
-    n,a,b = 0,0,1
-    while n<max:
-        yield b
-        a,b = b,a+b
-        n += 1
-    return 'done'
 
-for i in fib(10):
-    print(i)
 
