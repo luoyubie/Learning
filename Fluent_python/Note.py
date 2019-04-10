31719532eb8fb1f353458324346f23d417be36fa
luoyu <luoyu_bie@sina.com>
2019-04-10 16:39:03 +0800

    #具名元组#

--- Fluent_python/Note.py
+++ Fluent_python/Note.py
@@ -18,3 +18,23 @@ for i in fib(10):
     print(i)
 """
 
+"""
+# 具名元组
+# 1、创建一个具名元组需要两个参数，一个是类名，另一个是类的各个字段的名字。后者可以是由数个字符串组成的可迭代对象，或者是由空格分隔开的字段名组成的字符串
+# 2、存放在对应字段里的数据要以一串参数的形式传入到构造函数中
+# （注意，元组的构造函数却只接受单一的可迭代对象）。
+# 3、你可以通过字段名或者位置来获取一个字段的信息
+from collections import namedtuple
+City = namedtuple('City','name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+print(tokyo)
+# _fields 属性是一个包含这个类所有字段名称的元组
+print(City._fields)
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+# 用 _make() 通过接受一个可迭代对象来生成这个类的一个实例，它的作用跟 City(*delhi_data) 是一样的
+delhi = City._make(delhi_data)
+# _asdict() 把具名元组以 collections.OrderedDict 的形式返回，我们可以利用它来把元组里的信息友好地呈现出来
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
+"""
