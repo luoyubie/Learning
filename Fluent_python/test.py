49ca83bfc6dd5968d32e1a250c889956a75540a5
luoyu <luoyu_bie@sina.com>
2019-04-30 17:36:35 +0800

    #test#

--- Fluent_python/test.py
+++ Fluent_python/test.py
@@ -1,14 +1,30 @@
 # _*_ coding:utf-8 _*_
 # @Author: Bie
 # @Time: 2019年03月12日
-import dis
-import bisect
-import sys
-
-a = {}
-a.setdefault('a',[]).append(1)
-print(a.get('a'))
-# print(a)
 
 
+def tag(name, *content, cls=None, **attrs):
+    """生成一个或多个HTML标签"""
+    if cls is not None:
+        attrs['class'] = cls
+    if attrs:
+        attr_str = ''.join(' %s="%s"' % (attr, value)
+                            for attr, value
+                            in sorted(attrs.items()))
+    else:
+        attr_str = ''
+    if content:
+        return '\n'.join('<%s%s>%s</%s>' %
+                         (name, attr_str, c, name) for c in content)
+    else:
+        return '<%s%s />' % (name, attr_str)
+
+print(tag('br'))
+print(tag('br','hello','world'))
+print(tag('br','hello','world',id=33))
+print(tag('p', 'hello', 'world', cls='sidebar'))
+print(tag(content='testing', name="img"))
+my_tag = {'name': 'img', 'title': 'Sunset Boulevard',
+             'src': 'sunset.jpg', 'cls': 'framed'}
+print(tag(**my_tag))
 
