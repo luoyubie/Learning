32669335724b909f2d4eca95df4efe05c0355e40
luoyu <luoyu_bie@sina.com>
2019-06-17 10:34:29 +0800

    #123#

--- ML/KNN.py
+++ ML/KNN.py
@@ -11,8 +11,26 @@ def creatDataSet():
     return group,labels
 
 
+def classsify0(inX,dataSet,labels,k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX,(dataSetSize,1)) - dataSet
+    # print(inX)
+    # print(np.tile(inX,(dataSetSize,1)))
+    sqDiffMat = diffMat ** 2
+    sqDistance = sqDiffMat.sum(axis=1)
+    distances = sqDistance ** 0.5
+    sortDistance = distances.argsort()
+    # print(distances)
+    # print(sortDistance)
+    classCount = {}
+    for i in range(k):
+        votelabel = labels[sortDistance[i]]
+        classCount[votelabel] = classCount.get(votelabel,0) + 1
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    # print(sortedClassCount)
+    return sortedClassCount[0][0]
+
+
 group,labels = creatDataSet()
-print(group.shape)
-print(labels)
 
-
+print(classsify0([0,0],group,labels,3))
---
+++ ML/test.py
@@ -0,0 +1,7 @@
+# _*_ coding:utf-8 _*_
+# @Author: Bie
+# @Time: 2019年06月17日
+
+
+
+
