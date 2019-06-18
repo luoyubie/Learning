aa50b28c6ff5fb68639608569a46a6c780d55460
luoyu <luoyu_bie@sina.com>
2019-06-18 17:50:49 +0800

    #11#

--- ML/KNN.py
+++ ML/KNN.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import operator
+import os
 
 def creatDataSet():
     group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -73,10 +74,50 @@ def datingClassTest():
             error_num += 1
     print("The classifier error ratio is %f" % (error_num/float(testNum)))
 
+def img2vec(filename):
+    returnVector = np.zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        line = fr.readline()
+        for j in range(32):
+            returnVector[0,i*32+j] = int(line[j])
+    return returnVector
+
+def _get_Vec(path):
+    filenames = os.listdir(path)
+    labels = []
+    file_num = len(filenames)
+    dataSet = np.zeros((file_num, 1024))
+    for i in range(file_num):
+        filename = filenames[i]
+        labels.append(filename[0])
+        filepath = os.path.join(path, filename)
+        vec = img2vec(filepath)
+        dataSet[i, :] = vec
+    return dataSet,labels
+
+def Digital_Recognition():
+    path_train = "E:\code\Learning\ML\Data\Ch02\\trainingDigits"
+    train_dataSet,train_labels = _get_Vec(path_train)
+    path_test = "E:\code\Learning\ML\Data\Ch02\\testDigits"
+    test_dataSet,test_labels = _get_Vec(path_test)
+    error_num = 0.0
+    for i in range(test_dataSet.shape[0]):
+        predict_label = classsify0(test_dataSet[i,:],train_dataSet,train_labels,5)
+        print("The classifier came back label is %s,The real label is %s"%(predict_label,test_labels[i]))
+        if predict_label != test_labels[i]:
+            error_num += 1
+    print("The classifier error ratio is %f" % (error_num/float(len(test_labels))))
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    datingClassTest()
-
+    # datingClassTest()
+    Digital_Recognition()
 
 
 
