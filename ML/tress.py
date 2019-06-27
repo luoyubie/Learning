b7182f6c48ff22eed8fe9956e65049b6753ddbea
luoyu <luoyu_bie@sina.com>
2019-06-27 17:52:11 +0800

    #27#

--- ML/tress.py
+++ ML/tress.py
@@ -88,3 +88,20 @@ dataSet,labels = createDataSet()
 
 print(createTree(dataSet,labels))
 
+
+def classify(inputTree,featLabels,testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index[firstStr]
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key],featLabels,testVec)
+        else:
+            classLabel = secondDict[key]
+    return classLabel
+
+
+
+
+
