f517fdbe50b356a435ba6e36866f016b542e6b63
luoyu <luoyu_bie@sina.com>
2019-07-17 17:46:22 +0800

    #12#

--- ML/bayes.py
+++ ML/bayes.py
@@ -15,6 +15,25 @@ def loadDataSet():
     classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
     return postingList,classVec
 
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList,inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print("the word：%s is not in my Vocabulary!" % word)
+    return returnVec
+
+
+
+
+
 
 
 
