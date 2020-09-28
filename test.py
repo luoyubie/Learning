bce4cba08c0ce7d20e14fe1036a98799e86ea2a5
bie <luoyu_bie@sina.com>
2020-09-28 15:03:02 +0800

    #0928#

--- test.py
+++ test.py
@@ -2,6 +2,11 @@
 # @Author: Bie
 # @Time: 2019年05月20日
 
-
-
+import numpy as np
+a = np.repeat(np.arange(5).reshape([1,-1]),10,axis = 0)+10.0
+b = np.random.randint(5, size= a.shape)
+c = np.argmin(a*b, axis=1)
+b = np.zeros(a.shape)
+b[np.arange(b.shape[0]), c] = 1
+print(b)
 
