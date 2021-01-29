dd2aead542d082300879b348ef4f962250c36f0b
bie <luoyu_bie@sina.com>
2021-01-29 10:03:36 +0800

    #0129#

---
+++ data_analysis_mining/chapter3.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+# Author: luoyu
+# Date: 2021/1/20 21:31
+# Tool: PyCharm
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+atering_sale = "D:\PycharmProject\\analysis_mining_data\chapter3\demo\data\catering_sale.xls"
+
+data = pd.read_excel(atering_sale)
+print(data.describe())
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+plt.figure()
+p = data.boxplot(return_type='dict')
+print(p)
+x = p['fliers'][0].get_xdata() # fliers为异常值的标签
+y = p['fliers'][0].get_ydata()
+y.sort()
+for i in range(len(x)):
+    if i > 0:
+        # plt.annotate(y[i], xy=(x[i], y[i]), xytext=(x[i] + 0.05 - 0.8 / (y[i] - y[i - 1]), y[i]))
+        plt.annotate(y[i], xy=(x[i], y[i]), xytext=(x[i] + 0.05 - 0.8 / (y[i] - y[i - 1]), y[i]))
+    else:
+        plt.annotate(y[i], xy=(x[i], y[i]), xytext=(x[i] + 0.08, y[i]))
+plt.show()
---
+++ data_analysis_mining/test.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+# Author: luoyu
+# Date: 2021/1/21 10:00
+# Tool: PyCharm
+
+
+print(3026 + 1.5*(3026-2451))
+print(2451-1.5*(3026-2451))
+
+
