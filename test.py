daf44d125c6b3cb3b2d79708481d0163ac862a21
bie <luoyu_bie@sina.com>
2021-01-31 23:55:22 +0800

    #0131#

--- test.py
+++ test.py
@@ -3,11 +3,11 @@
 # @Time: 2019年05月20日
 
 import pandas as pd  # 导入数据分析库Pandas
-from scipy.interpolate import lagrange #导入拉格朗日插值函数
-import numpy as np
-x=[139, 140, 141, 142, 143, 145, 146, 147, 148, 149]
-y=[3193.4, 3075.4, 2847.6, 2311.4, 2327.3, 2616.6, 2620.2, 2616.4, 2655.8, 2310.7]
-z=lagrange(x,y)(144)
-print(z) #而https://www.easycalculation.com/lagrange-interpolation.php 在线计算插值结果为：2541.66
+# from scipy.interpolate import lagrange #导入拉格朗日插值函数
+# import numpy as np
+# x=[139, 140, 141, 142, 143, 145, 146, 147, 148, 149]
+# y=[3193.4, 3075.4, 2847.6, 2311.4, 2327.3, 2616.6, 2620.2, 2616.4, 2655.8, 2310.7]
+# z=lagrange(x,y)(144)
+# print(z) #而https://www.easycalculation.com/lagrange-interpolation.php 在线计算插值结果为：2541.66
 
-
+print(1-1e1)
